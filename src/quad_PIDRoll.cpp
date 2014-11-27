a225cd84849cffbf9fbfc3fb4d6b1fc96275e257
xroche <xavier.roche@open.eurogiciel.org>
2014-11-27 13:23:34 +0100

    test
    Signed-off-by: xroche <xavier.roche@open.eurogiciel.org>

--- src/quad_PIDRoll.cpp
+++ src/quad_PIDRoll.cpp
@@ -22,7 +22,7 @@ PIDRoll::~PIDRoll()
 void PIDRoll::m_init()
 {
 	m_error_prev = m_error = m_error_sum = 0.0;
-	m_Kp = 1.2;
+	m_Kp = 7.8;
 	m_Ki = 0.8;
 	m_Kd = 0.001;
 	m_value = m_target = m_output = m_dt = 0.0;
